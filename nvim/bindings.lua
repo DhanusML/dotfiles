04521bddb2c0c24a0f21ed53d8e179edc1fe9613
Dhanus M Lal <Dhanus.MLal@fujitsu.com>
2024-07-10 17:00:57 +0530

    updated config

--- nvim/bindings.lua
+++ nvim/bindings.lua
@@ -1,8 +1,66 @@
 -- bindings
-vim.keymap.set('i', 'jk', '<esc>')
+
+local function invert_hl ()
+    vim.opt.hlsearch = not vim.opt.hlsearch._value
+end
+
+-- Normal mode
 vim.keymap.set('n', '<leader>l', '<c-w>l')
 vim.keymap.set('n', '<leader>h', '<c-w>h')
 vim.keymap.set('n', '<leader>j', '<c-w>j')
 vim.keymap.set('n', '<leader>k', '<c-w>k')
 vim.keymap.set('n', '<leader>sv', ':source $MYVIMRC<cr>')
 vim.keymap.set('n', '<leader>ev', ':edit $MYVIMRC<cr>')
+vim.keymap.set('n', '<leader>n', invert_hl)
+
+-- Insert mode
+vim.keymap.set('i', 'jk', '<esc>')
+
+-- Autocorrect
+vim.cmd('inoreabbrev paht path')
+vim.cmd('inoreabbrev taht that')
+vim.cmd('inoreabbrev classificaton classification')
+vim.cmd('inoreabbrev compnents components')
+vim.cmd('inoreabbrev compnent component')
+vim.cmd('inoreabbrev pythone python')
+vim.cmd('inoreabbrev smaples samples')
+vim.cmd('inoreabbrev sover solver')
+vim.cmd('inoreabbrev souce source')
+
+-- Autocmds
+local common_group = vim.api.nvim_create_augroup('common', {clear = true})
+local spec_group = vim.api.nvim_create_augroup('lang_spec', {clear = true})
+
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = { "python", "make", "sh", "cmake" },
+    callback = function(args)
+        vim.keymap.set('v', '<leader>#', ':normal 0i#<esc>', { buffer = args.buf })
+    end,
+    group = 'lang_spec',
+    desc = "comment multiple lines for python source file"
+})
+
+
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = { 'c', 'cpp' },
+    callback = function(args)
+        vim.keymap.set('v', '<leader>//', ':normal 0i//<esc>', { buffer = args.buf })
+    end,
+    group = 'lang_spec',
+    desc = 'comment multiple lines for c/c++'
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = { 'c', 'cpp' },
+    callback = function(args)
+        vim.keymap.set('v', '<leader>/*', '<esc>`<i/*<esc>`>i*/<esc>', { buffer = args.buf })
+    end,
+    group = 'lang_spec',
+    desc = 'comment multiple lines for c/c++'
+})
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+    callback = function() vim.highlight.on_yank({ timeout = 1000 }) end,
+    group = 'common',
+    desc = "briefly highlight yanked text"
+})
--- nvim/settings.lua
+++ nvim/settings.lua
@@ -1,16 +1,18 @@
 -- global settings
 vim.g.mapleader = ' '
+vim.cmd.colorscheme('habamax')
 
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.softtabstop = 2
 vim.opt.expandtab = true
 
+vim.opt.hlsearch = false
+
 vim.opt.autoindent = true
 
 vim.opt.number = true
 vim.opt.relativenumber = true
-
 vim.opt.cursorline = true
 
-vim.opt.statusline = '%f %y %= C:%c L:%L %p%%'
+vim.opt.statusline = '%f %m %y %= C:%c L:%L %p%%'
